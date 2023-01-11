4d80018df23543c7ddaa7ee1294bf66373b3ee89
Cade Chaney <cadechaney@live.com>
2023-01-11 14:45:16 -0600

    Add ifstatements

--- ifStatements.js
+++ ifStatements.js
@@ -42,10 +42,22 @@ if (weather === "sunny") {
 // Experiment with manipulating the value held within numQuarters
 // to make sure both conditions can be achieved.
 
-var numQuarters = 0
+var numQuarters = 4
 
 console.log("I have enough money for a gumball");
 console.log("I don't have enough money for a gumball");
+
+
+if (numQuarters >= 2) {
+  console.log("I have enough money for a gumball");
+} else if (numQuarters < 2) {
+  console.log("I don't have enough money for a gumball");
+} 
+
+
+
+
+
 
 
 // #####################
@@ -62,6 +74,18 @@ console.log("I don't have enough money for a gumball");
 // Experiment with manipulating the value held within both variables
 // to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 3;
 var hasSauce = true;
+
+
+if (cupsOfFlour === 1 && hasSauce === true) {
+  console.log("I cannot make pizza");
+} else if (cupsOfFlour === 5 && hasSauce === false) {
+  console.log("I cannot make pizza");
+} else if (cupsOfFlour === 2 && hasSauce === true) {
+  console.log("I can make pizza");
+} else if (cupsOfFlour === 3 && hasSauce === true) {
+  console.log("I can make pizza");
+} 
+
 
