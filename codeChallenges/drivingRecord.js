fd48e510b55974da20b43b82f855294829c14bb8
Cade Chaney <cadechaney@live.com>
2023-01-11 15:50:54 -0600

    Fix syntax error

--- codeChallenges/drivingRecord.js
+++ codeChallenges/drivingRecord.js
@@ -4,10 +4,10 @@ var goodDrivingRecord = true
 var age = 24
 
 
-if (goodDrivingRecord === true && age >= 25) {
+if (goodDrivingRecord === true && age >=25) {
   console.log("Should get a discount on Car Rental");
-} else if (goodDrivingRecord === true || age >= 25) {
+} else if (goodDrivingRecord === true || age > 25) {
   console.log("Should pay full price");
-} else  (goodDrivingRecord === false && age < 25) {
+} else  {
   console.log("Someone else needs to sign for rental");
-} 
+}
