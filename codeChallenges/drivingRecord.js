b4619c34841a6e2837d6c753a43efee7b6079b91
Cade Chaney <cadechaney@live.com>
2023-01-11 15:54:20 -0600

    Correct equal to error

--- codeChallenges/drivingRecord.js
+++ codeChallenges/drivingRecord.js
@@ -6,7 +6,7 @@ var age = 24
 
 if (goodDrivingRecord === true && age >=25) {
   console.log("Should get a discount on Car Rental");
-} else if (goodDrivingRecord === true || age > 25) {
+} else if (goodDrivingRecord === true || age >= 25) {
   console.log("Should pay full price");
 } else  {
   console.log("Someone else needs to sign for rental");
