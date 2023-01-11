711c6163a315411ab9fd1d585a111538e16d36b3
Cade Chaney <cadechaney@live.com>
2023-01-11 15:47:14 -0600

    Finish assignment

--- codeChallenges/drivingRecord.js
+++ codeChallenges/drivingRecord.js
@@ -1,13 +1,13 @@
 
 
 var goodDrivingRecord = true
-var age = 2
+var age = 24
 
 
 if (goodDrivingRecord === true && age >= 25) {
   console.log("Should get a discount on Car Rental");
 } else if (goodDrivingRecord === true || age >= 25) {
   console.log("Should pay full price");
-} else if (goodDrivingRecord === false && age < 25) {
+} else  (goodDrivingRecord === false && age < 25) {
   console.log("Someone else needs to sign for rental");
 } 
